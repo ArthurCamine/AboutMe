5c69477f37955673eae7127b6cb185496b48e250
Arthur Rodrigues Camine <arthur.camine@satrotas.com.br>
2020-03-29 20:39:04 -0300

    Adding footer and css fixes

--- js/contact.js
+++ js/contact.js
@@ -1,4 +1,6 @@
 var btnSend = document.querySelector("#btnSend");
+var form = document.querySelector("form");
+var errorList = document.querySelector(".errors");
 
 btnSend.addEventListener("click", function() {
     var formObject = {
@@ -9,6 +11,27 @@ btnSend.addEventListener("click", function() {
 
     fetch("https://arthurcamine-contactform.azurewebsites.net/api/ContactFormFunction", {
         method: "POST",
-        body: formObject
-    });
+        body: JSON.stringify(formObject)
+    }).then(response => {
+        if (response.status == 400) {
+            errorList.innerHTML = "";
+            return response.json().then(jsonResponse => {
+                jsonResponse.errors.forEach(errors => {
+                    var li = document.createElement("li");
+                    li.innerHTML = errors;
+                    li.classList.add("error");
+                    errorList.appendChild(li);
+                });
+            });
+        } else if (response.status == 200) {
+            console.log("Message sent");
+            errorList.innerHTML = "";
+            form.reset();
+        } else {
+            var li = document.createElement("li");
+            li.innerHTML = "Something went wrong sending your message.";
+            li.classList.add("error");
+            errorList.appendChild(li);
+        }
+    })
 });
